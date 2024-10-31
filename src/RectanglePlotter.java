d4d22a03750080e102cb2e16740455513b9dff5a
Paolo Salvatierra <psalvatierra@outlook.com>
2024-10-31 08:32:19 -0700

    Grid demo

--- src/RectanglePlotter.java
+++ src/RectanglePlotter.java
@@ -1,6 +1,11 @@
+import GridWorld.Grid;;
+
 public class RectanglePlotter {
     public static void main(String[] args)
     {
         System.out.println("Hello, World!");
+        Grid paper = Grid.create(4, 10, '.');
+        paper.mark(2, 3, 'b');
+        paper.dump();
     }
 }
